447c0e51f6de469cb5425919c4196a5e19728fd7
tmelmer <76037158+remlem5@users.noreply.github.com>
2021-10-08 09:08:59 +0200

    Aufgabe 2: Video bis Minute 29

---
+++ 04_Fullstack/Aufgabe 2/src/main/java/com/itkolleg/SpringBootAufgabe2Application.java
@@ -0,0 +1,13 @@
+package com.itkolleg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAufgabe2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAufgabe2Application.class, args);
+    }
+
+}

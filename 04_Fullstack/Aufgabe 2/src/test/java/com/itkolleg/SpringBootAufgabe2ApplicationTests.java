49cbc81a44430d51c5600c7a8638def168b6d0be
remlem5 <tmelmer@tsn.at>
2021-10-08 09:04:52 +0200

    Aufgabe 2: Video bis Minute 29

---
+++ 04_Fullstack/Aufgabe 2/src/test/java/com/itkolleg/SpringBootAufgabe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itkolleg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAufgabe2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
